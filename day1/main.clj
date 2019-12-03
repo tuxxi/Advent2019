1b641a9534d8460e561d5e40d05d09cc41141018
Aidan Sojourner <aidan.sojourner@gmail.com>
2019-12-02 20:05:04 -0800

    Day 1, Part 2. Also refactored part1 quite a bit!

--- day1/main.clj
+++ day1/main.clj
@@ -1,10 +1,23 @@
-(ns advent2019.day1
-  (:gen-class))
+(ns advent2019.day1)
 
-(defn day1 [accum mass]
-  (let [m (Integer/parseInt mass)]
-    (+ accum (- (int (/ m 3)) 2 ))))
+(defn parse [filename]
+  (mapv #(Integer/parseInt %) 
+        (clojure.string/split-lines (slurp filename))))
 
-;; open the file "input" and call our day1 func on each line
-(with-open [rdr (clojure.java.io/reader "input")]
-  (println (reduce day1 0 (line-seq rdr))))
+(defn calc-fuel [mass]
+  (-> mass (/ 3) (int) (- 2))) 
+
+(defn calc-total-fuel [mass]
+  (apply + (take-while pos? (iterate calc-fuel (calc-fuel mass)))))
+
+(defn day1-1 [filename]
+    (apply + (mapv calc-fuel (parse filename))))
+
+(defn day1-2 [filename]
+    (apply + (mapv calc-total-fuel (parse filename))))
+
+;; main
+(println "Day 1, Part 1:")
+(println (day1-1 "input"))
+(println "Part 2:")
+(println (day1-2 "input"))
